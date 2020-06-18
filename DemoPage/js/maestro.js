c6d76ca3409d037a15aa124b42699170557dca78
jessyjones <alya@moov.mg>
2020-06-18 17:16:24 +0200

    Affichage de l'heure

--- DemoPage/js/maestro.js
+++ DemoPage/js/maestro.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", faireDesChoses);
 
+//Crééer les variables
 let endroitHeureSurPage ;
 let heureActuelle ;
 let heureTexte = "";
+let deadline;
+let diff;
+let countdown;
 
 function faireDesChoses()
 {
@@ -11,10 +15,14 @@ function faireDesChoses()
 
 function afficherHeure()
 {
+  deadline = new Date("Jul 2,2020 17:00:00")//Heure cible
   endroitHeureSurPage = document.getElementById('compteur')
+diff = deadline - heureActuelle;
+//console.log(diff)
   heureActuelle = new Date();
+  countdown = new Date(diff);
   //console.log(endroitHeureSurPage)
-  heureTexte = heureActuelle.getHours() + ":" + heureActuelle.getMinutes() + ":" + heureActuelle.getSeconds() ;
+  heureTexte = countdown.getDate() + "J " + countdown.getHours() + ":" + countdown.getMinutes() + ":" + countdown.getSeconds() ;
   endroitHeureSurPage.innerText = heureTexte;
 
 }
